45da6728601b94ca646d3ba968785561555b3765
Himanshu Bhatt <146898425+himanshubhatt-23@users.noreply.github.com>
2026-07-16 01:53:24 +0530

    Time: 0 ms (100%), Space: 71.6 MB (98.74%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int result = nums[0];
+        int total = 0;
+        for (int n : nums) {
+            if (total < 0) {
+                total = 0;
+            }
+            total += n;
+            result = max(result, total);
+        }
+        return result;        
+    }
+};
